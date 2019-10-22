94eb5cd5cf7aa785c9962876aaae120b5d87af93
huangk10 <huangk10@126.com>
2019-10-22 10:57:47 +0800

    init setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scheduler',
+    version='0.0.1',
+    packages=['scheduler'],
+    url='https://github.com/huangk10/Scheduler',
+    license='MIT',
+    author='huangk',
+    author_email='huangk10@126.com',
+    description='A concurrent api for human',
+)
